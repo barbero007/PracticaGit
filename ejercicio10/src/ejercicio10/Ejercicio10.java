7240301b5f31121819023c231c475b8c33096f34
Barbero <Barbero@alejandro>
2015-10-22 17:34:25 +0200

    Esto es el commit del proyecto creado por el administrador.

---
+++ ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio10;
+
+/**
+ *
+ * @author Barbero
+ */
+public class Ejercicio10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int numero = 0;
+        int contador = 0;
+        int numero_aleatorio;
+        do {
+            numero_aleatorio = (int) ((Math.random()) * 100 + 1);
+            if (numero_aleatorio % 2 != 0) {
+                System.out.println(numero_aleatorio);
+                numero++;
+            }
+            contador++;
+        } while (numero < 3);
+
+        System.out.println("Cantidad de valores aleatorios: " + contador);
+    }
+    
+}
